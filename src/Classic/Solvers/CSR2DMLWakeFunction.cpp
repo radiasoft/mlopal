1a13bafe77cdbe4a4eb250f58358926b5096ac21
Evan Carlin <evan@carlin.com>
2024-06-05 20:35:54 +0000

    ckp

--- src/Classic/Solvers/CSR2DMLWakeFunction.cpp
+++ src/Classic/Solvers/CSR2DMLWakeFunction.cpp
@@ -13,6 +13,8 @@
 // along with OPAL. If not, see <https://www.gnu.org/licenses/>.
 //
 #include "Solvers/CSR2DMLWakeFunction.h"
+#include <pybind11/embed.h>
+namespace py = pybind11;
 
 // TODO(e-carlin): needed?
 #include "AbstractObjects/OpalData.h"
@@ -28,7 +30,18 @@ CSR2DMLWakeFunction::CSR2DMLWakeFunction(const std::string& name,
 void CSR2DMLWakeFunction::apply(PartBunchBase<double, 3>* bunch) {
     Inform msg("MLWake ");
     // TODO(e-carlin): call model
-    std::cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+    py::scoped_interpreter guard{}; // start the interpreter
+
+    py::module sys = py::module::import("sys");
+    sys.attr("path").attr("append")("."); // add the current directory to Python's sys.path
+
+    py::module py_module = py::module::import("py_module"); // import the python module
+    py::function py_function = py_module.attr("py_function"); // get the python function
+
+    int a = 5, b = 10;
+    py::object result = py_function(a, b); // call the python function
+
+    std::cout << "xxxxxxxxxxxxxxxxxx The result is: " << result.cast<int>() << std::endl; // print the result
 }
 
 void CSR2DMLWakeFunction::initialize(const ElementBase* ref) {
