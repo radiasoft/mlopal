d24765adfa4db6222da17e1593b1b5861e9857a2
Evan Carlin <evan@carlin.com>
2024-06-11 15:10:31 +0000

    ckp - running specifying file from opal.in as a filesystem::path

--- src/Classic/Solvers/CSR2DMLWakeFunction.cpp
+++ src/Classic/Solvers/CSR2DMLWakeFunction.cpp
@@ -34,14 +34,14 @@ void CSR2DMLWakeFunction::apply(PartBunchBase<double, 3>* bunch) {
     std::cout << "111111111111111111111111111111111" << pyFilepath_m << std::endl;
     py::scoped_interpreter guard{}; // start the interpreter
     // // TODO(e-carlin): call model
-    std::cout << "22222222222222222222222222222222222222" << std::endl;
-    py::print("Hello, World!");
+    std::cout << "22222222222222222222222222222222222222" << pyFilepath_m.parent_path() << std::endl;
     py::module sys = py::module::import("sys");
-    sys.attr("path").attr("append")("/home/vagrant/src/radiasoft/mlopal/egc/example/");
-    std::cout << "33333333333333333333333333333333333" << std::endl;
-    py::module py_module = py::module::import("py_module"); // import the python module
+    sys.attr("path").attr("append")(pyFilepath_m.parent_path().string().c_str());
+    std::cout << "33333333333333333333333333333333333" << pyFilepath_m.stem() << std::endl;
+    py::module py_module = py::module::import(pyFilepath_m.stem().string().c_str()); // import the python module
     std::cout << "55555555555555555555555555555555555" << std::endl;
-    py::function py_function = py_module.attr("py_function"); // get the python function
+    // TODO(e-carlin): change the name of the func
+    py::function py_function = py_module.attr("get_wake"); // get the python function
     std::cout << "66666666666666666666666666666666666" << std::endl;
     int a = 5, b = 10;
     py::object result = py_function(a, b); // call the python function
